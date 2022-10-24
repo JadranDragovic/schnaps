6d6cb07dae116846a986930ba25996932d00b5bb
bobovecdamjan <106660889+bobovecdamjan@users.noreply.github.com>
2022-10-24 23:06:50 +0200

    Update pygame ucenje.py

--- pygame ucenje.py
+++ pygame ucenje.py
@@ -1,4 +1,5 @@
 import pygame 
+from pygame import Surface
 import random
 import os
 
@@ -48,8 +49,9 @@ p2inv = []
 adut = []
 p1bodovi_runda = []
 p2bodovi_runda = []
+usporedba = []
 
-def dijeljenje_karata():
+def dijeljenje_karata():#podijeli random karte igračima
 	for i in range(3):
 		p1inv.append(random.choice(dek))
 		dek.remove(p1inv[i])
@@ -65,11 +67,11 @@ def dijeljenje_karata():
 		p2inv.append(random.choice(dek))
 		dek.remove(p2inv[i])
 
-def draw_text(text,font,text_col,x,y):
+def draw_text(text,font,text_col,x,y):#služi za prikazivanje bilo kakavog teksta na ekranu
 	img = font.render(text,True,text_col)
 	screen.blit(img,(x,y))
 
-class Button():
+class Button():#proizvodim vlastitu klasu za gumb 
 	def __init__(self, x, y, image,scale):
 		self.image = image
 		width = image.get_width()
@@ -79,9 +81,11 @@ class Button():
 		self.rect.topleft = (x, y)
 		self.clicked = False
 
-	def draw(self):
+	def draw(self,):
+
+		
 		action = False
-		#dobivamo mouse position
+			#dobivamo mouse position
 		pos = pygame.mouse.get_pos()
 		if self.rect.collidepoint(pos):
 			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
@@ -104,7 +108,11 @@ btn_zavrsi_bacanje = Button(512,70,sedam_bodova_slika,1)
 
 menu_state ="main" #pomaže za mijenjanje prozora
 karte_state ="ne_prikaz"
-
+sakri_kartu ="ne"
+sakri_kartu2 ="keks"
+promjena_reda = 0
+prekri_kartu = pygame.Surface((200,500))
+prekri_kartu2 = pygame.Surface((200,500))
 
 def kliknut_sedam():#window za igru od 7 bodova
 	run = True
@@ -116,49 +124,114 @@ def kliknut_sedam():#window za igru od 7 bodova
 				run = False
 		global ciji_red
 		global karte_state
+		global promjena_reda
+		global prekri_kartu
+		global sakri_kartu
+		global sakri_kartu2
+		global p1bodovi_runda
+		global p2bodovi_runda
 		screen.fill(zelena)
 		p1bodovi = 7
 		p2bodovi = 7
 		draw_text(f"{igrač1ime}:{p1bodovi}",font2,text_color,880,10)
 		draw_text(f"{igrač2ime}:{p2bodovi}",font2,text_color,880,50)
-		if btn_podijeli_karte.draw() == True:
+
+		if btn_podijeli_karte.draw() == True:#ako se klikne taj gumb onda se podijeli karte s pomocu funkcije dijeljenje_karata
 			dijeljenje_karata()
 			karte_state = "prikaz"
 
-		if karte_state == "prikaz":
+		if karte_state == "prikaz":#pomaze mi samo da prikazujem ili ne prikazujem kartu
 			#prikaz karata
-			p1karta_1 = Button(50,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[0]]).convert_alpha(),0.2).draw()
-			p1karta_2 = Button(250,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[1]]).convert_alpha(),0.2).draw()
-			p1karta_3 = Button(450,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[2]]).convert_alpha(),0.2).draw()
-			p1karta_4 = Button(650,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[3]]).convert_alpha(),0.2).draw()
-			p1karta_5 = Button(850,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[4]]).convert_alpha(),0.2).draw()
+			if btn_zavrsi_bacanje.draw() == True:#ako se klikne gumb zavrsi bacanje promijeni se graficki prikaz inventorya igraca
+				promjena_reda += 1
 
-			p2karta_1 = Button(1425,400,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[0]]).convert_alpha(),0.2).draw()
-			p2karta_2 = Button(1425,400,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[1]]).convert_alpha(),0.2).draw()
-			p2karta_3 = Button(1425,400,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[2]]).convert_alpha(),0.2).draw()
-			p2karta_4 = Button(1425,400,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[3]]).convert_alpha(),0.2).draw()
-			p2karta_5 = Button(1425,400,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[4]]).convert_alpha(),0.2).draw()
+			if len(usporedba) != 2:
+				if promjena_reda %2 == 0:
+					p1karta_1 = Button(50,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[0]]).convert_alpha(),0.2).draw()
+					if p1karta_1 == True:
+						usporedba.append(p1inv[0])
+						prekri_kartu.fill(zelena)
+						sakri_kartu = "da"
+					if sakri_kartu == "da":
+						screen.blit(prekri_kartu, (50,500))
+					p1karta_2 = Button(250,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[1]]).convert_alpha(),0.2).draw()
+					if p1karta_2 == True:
+						usporedba.append(p1inv[1])
+						prekri_kartu.fill(zelena)
+						sakri_kartu = "da1"
+					if sakri_kartu == "da1":
+						screen.blit(prekri_kartu, (250,500))
+					p1karta_3 = Button(450,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[2]]).convert_alpha(),0.2).draw()
+					if p1karta_3 == True:
+						usporedba.append(p1inv[2])
+						prekri_kartu.fill(zelena)
+						sakri_kartu = "daa"
+					if sakri_kartu == "daa":
+						screen.blit(prekri_kartu, (450,500))
+					p1karta_4 = Button(650,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[3]]).convert_alpha(),0.2).draw()
+					if p1karta_4 == True:
+						usporedba.append(p1inv[3])
+						prekri_kartu.fill(zelena)
+						sakri_kartu = "nee"
+					if sakri_kartu == "nee":
+						screen.blit(prekri_kartu, (650,500))
+					p1karta_5 = Button(850,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p1inv[4]]).convert_alpha(),0.2).draw()
+					if p1karta_5 == True:
+						usporedba.append(p1inv[4])
+						prekri_kartu.fill(zelena)
+						sakri_kartu = "daaa"
+					if sakri_kartu == "daaa":
+						screen.blit(prekri_kartu, (850,500))
+				else:
+					p2karta_1 = Button(50,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p2inv[0]]).convert_alpha(),0.2).draw()
+					if p2karta_1 == True:
+						usporedba.append(p2inv[0])
+						prekri_kartu2.fill(zelena)
+						sakri_kartu2 = "b"
+					if sakri_kartu2 == "b":
+						screen.blit(prekri_kartu2, (50,500))
+					p2karta_2 = Button(250,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p2inv[1]]).convert_alpha(),0.2).draw()
+					if p2karta_2 == True:
+						usporedba.append(p2inv[1])
+						prekri_kartu2.fill(zelena)
+						sakri_kartu2 = "bb"
+					if sakri_kartu2 == "bb":
+						screen.blit(prekri_kartu2, (250,500))
+					p2karta_3 = Button(450,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p2inv[2]]).convert_alpha(),0.2).draw()
+					if p2karta_3 == True:
+						usporedba.append(p2inv[2])
+						prekri_kartu2.fill(zelena)
+						sakri_kartu2 = "bbb"
+					if sakri_kartu2 == "bbb":
+						screen.blit(prekri_kartu2, (450,500))
+					p2karta_4 = Button(650,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p2inv[3]]).convert_alpha(),0.2).draw()
+					if p2karta_4 == True:
+						usporedba.append(p2inv[3])
+						prekri_kartu2.fill(zelena)
+						sakri_kartu2 = "bbbb"
+					if sakri_kartu2 == "bbbb":
+						screen.blit(prekri_kartu2, (650,500))
+					p2karta_5 = Button(850,500,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[p2inv[4]]).convert_alpha(),0.2).draw()
+					if p2karta_5 == True:
+						usporedba.append(p2inv[4])
+						prekri_kartu2.fill(zelena)
+						sakri_kartu2 = "b1"
+					if sakri_kartu2 == "b1":
+						screen.blit(prekri_kartu2, (850,500))
+			elif len(usporedba) == 2:
+				if vrijednosti_karata[usporedba[0]]>vrijednosti_karata[usporedba[1]]:
+					p1bodovi_runda.append(vrijednosti_karata[usporedba[0]]+vrijednosti_karata[usporedba[1]])
+					usporedba.clear()
+					print(p1bodovi_runda,p2bodovi_runda)
+				elif vrijednosti_karata[usporedba[0]]<vrijednosti_karata[usporedba[1]]:
+					p2bodovi_runda.append(vrijednosti_karata[usporedba[0]]+vrijednosti_karata[usporedba[1]])
+					usporedba.clear()
+					print(p1bodovi_runda,p2bodovi_runda)
+				else:
+					print("Neš ne radi")
+					usporedba.clear()
+					print(p1bodovi_runda,p2bodovi_runda)
 
-			if btn_zavrsi_bacanje.draw() == True:
-				p2karta_2.rect.topleft(100,100)
-
-
-
-				
-			#else:
-				#p1karta_1.(1425,400)
-				#p1karta_2.(1425,400)
-				#p1karta_3.(1425,400) 
-				#p1karta_4.(1425,400)
-				#p1karta_5.(1425,400)
-
-				#p2karta_1.(50,400) 
-				#p2karta_2.(250,400) 
-				#p2karta_3.(450,400)  
-				#p2karta_4.(650,400)  
-				#p2karta_5.(850,400)   
-
-				
 		pygame.display.update()
 	pygame.quit()
 

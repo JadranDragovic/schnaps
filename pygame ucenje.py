b5b8ca605cac5fb6b6bb81e452a4ed6ddc34e79a
JadranDragovic <107123503+JadranDragovic@users.noreply.github.com>
2022-10-31 17:58:36 +0100

    Dodano zatvaranje + promijena izgleda igre
    Opcija za zatvaranje igre je dodana (to ukljućuje gumb za tu opciju, pravila, te bodovanje za zatvaranje). Mnogi gumbovi sada imaju drugaciju poziciju i ograniceni su (npr. dijeljenje karti na pocetku sada nestane). Mnogi gumbovi imaju nove slike. Dodana je pozadina. Dodan je main menu, promijenjen je kod za micanje izmedu prozora, te je dodan gumb "vrati se natrag".

--- pygame ucenje.py
+++ pygame ucenje.py
@@ -2,8 +2,8 @@ import pygame
 import random
 import time
 from pygame import Surface
-pygame.init()
 
+pygame.init()
 igrač1ime = input("Unesi ime: ").upper()
 while len(igrač1ime)>7 or len(igrač1ime)==0:
     igrač1ime = input("Unesi ime koje ima manje od 7 znakova i više od 0: ").upper()
@@ -11,7 +11,7 @@ igrač2ime = input("Unesi ime: ").upper()
 while len(igrač2ime)>7 or len(igrač2ime)==0:
     igrač2ime = input("Unesi ime koje ima manje od 7 znakova i više od 0: ").upper()
 
-zelena = (0,100,0)
+#velicine
 screen = pygame.display.set_mode((1024,768))
 pygame.display.set_caption("ŠNAPS")
 FPS = 60
@@ -27,7 +27,14 @@ devet_bodova_slika = pygame.image.load("Desktop\projekt\devet.png").convert_alph
 povratak_btn_slika = pygame.image.load("Desktop\projekt\povratak_btn.png").convert_alpha()
 promijeni_red_btn_slika = pygame.image.load("Desktop\projekt\promijeni_btn.png").convert_alpha()
 pravila_slika = pygame.image.load("Desktop\projekt\pravila_btn.png").convert_alpha()
+igra_slika = pygame.image.load("Desktop\projekt\igra_btn.png").convert_alpha()
+izlaz_slika = pygame.image.load("Desktop\projekt\izlaz_btn.png").convert_alpha()
 kraj_runde_slika = pygame.image.load("Desktop\projekt\kraj_runde_table.png").convert_alpha()
+background_slika = pygame.image.load("Desktop\projekt\pozadina.png").convert_alpha()
+mainMenu_slika = pygame.image.load("Desktop\projekt\mainScreen.png").convert_alpha()
+zatvaranje_slika = pygame.image.load("Desktop\projekt\zatvaranje_btn.png").convert_alpha()
+zvanje_slika = pygame.image.load("Desktop\projekt\zvanje_btn.png").convert_alpha()
+kraj_slika = pygame.image.load("Desktop\projekt\kraj_btn.png").convert_alpha()
 
 #liste
 vrijednosti_karata = {"JC":2, "QC":3, "KC":4, "10C":10, "AC":11,"JS":2, "QS":3, "KS":4, "10S":10, "AS":11,"JD":2, "QD":3, "KD":4, "10D":10, "AD":11,"JH":2, "QH":3, "KH":4, "10H":10, "AH":11}
@@ -65,6 +72,7 @@ usporedba_red_bacanja2 = []
 bacena_kartap1 = []
 bacena_kartap2 = []
 zvanje_baceno = []
+zadnja_karta = []
 
 def dijeljenje_karata():#podijeli random karte igračima
 	for i in range(3):
@@ -86,7 +94,9 @@ def draw_text(text,font,text_col,x,y):#služi za prikazivanje bilo kakavog tekst
 	screen.blit(img,(x,y))
 
 class Button():#proizvodim vlastitu klasu za gumb 
+	hovered = False
 	def __init__(self, x, y, image,scale):
+		
 		self.image = image
 		width = image.get_width()
 		height = image.get_height()
@@ -95,9 +105,7 @@ class Button():#proizvodim vlastitu klasu za gumb
 		self.rect.topleft = (x, y)
 		self.clicked = False
 
-	def draw(self,):
-
-		
+	def draw(self,):		
 		action = False
 			#dobivamo mouse position
 		pos = pygame.mouse.get_pos()
@@ -110,16 +118,10 @@ class Button():#proizvodim vlastitu klasu za gumb
 			
 		screen.blit(self.image,(self.rect.x,self.rect.y))
 		return action
-
+	
 #buttoni
-btn_sedam_bodova = Button(300,240,sedam_bodova_slika,1)		
-btn_devet_bodova = Button(600,240,devet_bodova_slika,1)
-btn_povratak = Button(40,50,povratak_btn_slika,1)
-btn_pravila = Button(430,550,pravila_slika,0.7)
-btn_podijeli_karte = Button(800,350,sedam_bodova_slika,0.5)
-btn_zavrsi_bacanje = Button(460,80,promijeni_red_btn_slika,1)
+btn_zavrsi_bacanje = Button(460,80,promijeni_red_btn_slika,0.5)
 btn_izvuci_kartu = Button(800,250,sedam_bodova_slika,0.5)
-btn_zvanje = Button(200,350,sedam_bodova_slika, 0.5)
 
 #varijable
 menu_state ="main" #pomaže za mijenjanje prozora
@@ -137,9 +139,14 @@ p1pobjeda = 0
 p2pobjeda = 0
 brojac_vrsteP1 = 0
 brojac_vrsteP2 = 0
+zatvara = "0"
+zatvaranje_tekst = ""
 stih = 0
 brojac_ibera = 0
 brojac_ibera2 = 0
+
+#background
+screen.blit(background_slika, (0,0))
 
 def igra():#window za igru od 7 bodova
 	run = True
@@ -168,26 +175,24 @@ def igra():#window za igru od 7 bodova
 		global stari_bodovi2
 		global brojac_vrsteP1
 		global brojac_vrsteP2
+		global zatvara
+		global zatvaranje_tekst
+		global zadnja_karta
 		global brojac_ibera
 		global brojac_ibera2
 		global stih
-		screen.fill(zelena)
+		screen.blit(background_slika, (0,0))
 		draw_text(f"{igrač1ime}:{p1bodovi}",font2,text_color,880,10)
 		draw_text(f"{igrač2ime}:{p2bodovi}",font2,text_color,880,50)
 
-		if btn_podijeli_karte.draw() == True:#ako se klikne taj gumb onda se podijeli karte s pomocu funkcije dijeljenje_karata
-			while btn_podijeli_karte_crtaj % 2 == 0:
-				dijeljenje_karata()
-				karte_state = "prikaz"
-				btn_podijeli_karte_crtaj += 1
-		
-		#tipka za zvanje
-		if btn_zvanje.draw() == True:
-			menu_state ="zvanje"
-
-		#drzi menu zvanje otvorenim
-		if menu_state == "zvanje":
-			zvanje()
+		#dijeljenje karti
+		if len(p1inv) == 0:
+			btn_podijeli_karte = Button(500,350,sedam_bodova_slika,0.5)
+			if btn_podijeli_karte.draw() == True:#ako se klikne taj gumb onda se podijeli karte s pomocu funkcije dijeljenje_karata
+				while btn_podijeli_karte_crtaj % 2 == 0:
+					dijeljenje_karata()
+					karte_state = "prikaz"
+					btn_podijeli_karte_crtaj += 1
 
 		if karte_state == "prikaz":#pomaze mi samo da prikazujem ili ne prikazujem kartu
 			#prikaz karata
@@ -297,7 +302,7 @@ def igra():#window za igru od 7 bodova
 									
 				if len(karte_crtanje) == 1 or len(karte_crtanje) == 2:
 					if bacena_karta_state =="da":
-						bacena_karta = Button(400,250,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[karte_crtanje[-1]]).convert_alpha(),0.2).draw()
+						bacena_karta = Button(450,250,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[karte_crtanje[-1]]).convert_alpha(),0.2).draw()
 				
 				if promjena_reda == 1:
 					draw_text(f"{igrač2ime} JE NA REDU",font2,(255,255,255),50,10)
@@ -396,7 +401,7 @@ def igra():#window za igru od 7 bodova
 								
 				if len(karte_crtanje) == 1 or len(karte_crtanje) == 2:
 					if bacena_karta_state2 =="da":
-						bacena_karta = Button(400,250,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[karte_crtanje[-1]]).convert_alpha(),0.2).draw()
+						bacena_karta = Button(450,250,pygame.image.load("Desktop\projekt\slike\\"+ slike_karata[karte_crtanje[-1]]).convert_alpha(),0.2).draw()
 			
 				if btn_zavrsi_bacanje.draw() == True:#ako se klikne gumb zavrsi bacanje promijeni se graficki prikaz inventorya igraca
 					print("KLIKNUT SAM")
@@ -419,34 +424,36 @@ def igra():#window za igru od 7 bodova
 					print(usporedba_red_bacanja1,usporedba_red_bacanja2)
 					if zavrsi_bacanje_kliknut == 0:
 						promjena_reda =1
-						pygame.draw.rect(screen,zelena, pygame.Rect(0,0,1024,768))
-						draw_text(f"{igrač2ime} MOŽE IGRATI ZA 5 SEKUNDI",font,(255,255,255),110,330)
+						screen.blit(background_slika, (0,0))
+						draw_text(f"{igrač2ime} MOŽE IGRATI ZA 3 SEKUNDI",font,(255,255,255),110,330)
 						pygame.display.update()
-						time.sleep(5)
+						time.sleep(3)
 						zavrsi_bacanje_kliknut = 1
 					if zavrsi_bacanje_kliknut == 2:
 						if usporedba_red_bacanja1[-1] == "0":
 							promjena_reda = 0
-							pygame.draw.rect(screen,zelena, pygame.Rect(0,0,1024,768))
-							draw_text(f"{igrač1ime} MOŽE IGRATI ZA 5 SEKUNDI",font,(255,255,255),110,330)
+							screen.blit(background_slika, (0,0))
+							draw_text(f"{igrač1ime} MOŽE IGRATI ZA 3 SEKUNDI",font,(255,255,255),110,330)
 							pygame.display.update()
-							time.sleep(5)
+							time.sleep(3)
 							ogranici_bacanje1 = 0
 							ogranici_bacanje2 = 1
 							usporedba_red_bacanja1.append("1")
 							usporedba_red_bacanja2.append("1")
 						elif usporedba_red_bacanja2[-1] == "1":
 							promjena_reda = 1
-							pygame.draw.rect(screen,zelena, pygame.Rect(0,0,1024,768))
-							draw_text(f"{igrač2ime} MOŽE IGRATI ZA 5 SEKUNDI",font,(255,255,255),110,330)
+							screen.blit(background_slika, (0,0))
+							draw_text(f"{igrač2ime} MOŽE IGRATI ZA 3 SEKUNDI",font,(255,255,255),110,330)
 							pygame.display.update()
-							time.sleep(5)
+							time.sleep(3)
 							ogranici_bacanje1 = 0
 							ogranici_bacanje2 = 1
 							usporedba_red_bacanja2.append("0")
 							usporedba_red_bacanja1.append("0")
 
 			elif len(usporedba) == 2:
+				if len(p1inv) == 1 and len(p2inv) == 1 and sum(p1bodovi_runda) == sum(p2bodovi_runda):
+					zadnja_karta.append(usporedba)
 				if karte_crtanje[-2] ==  bacena_kartap1[-1] and karte_crtanje[-1] == bacena_kartap2[-1]:
 					if vrijednosti_karata[usporedba[0]]>vrijednosti_karata[usporedba[1]]:
 						p1bodovi_runda.append(vrijednosti_karata[usporedba[0]]+vrijednosti_karata[usporedba[1]])
@@ -491,10 +498,33 @@ def igra():#window za igru od 7 bodova
 						stih = 1
 						usporedba.clear()
 						print(p1bodovi_runda,p2bodovi_runda)
-		if len(p1inv) == 0 and len(p2inv) == 0 and len(dek) == 0:
-			print("gotova runda")
-		if sum(p1bodovi_runda) >= 66 or sum(p2bodovi_runda) >= 66:
-			kraj_runde_btn = Button(200,250,sedam_bodova_slika, 0.5)
+		
+		#tipka za zvanje
+		if len(p1inv) > 1 or len(p2inv) > 1:
+			btn_zvanje = Button(100,265,zvanje_slika, 0.5)
+			if btn_zvanje.draw() == True:
+				menu_state ="zvanje"
+			#drzi menu zvanje otvorenim
+			if menu_state == "zvanje":
+				zvanje()	
+
+		#ZATVARANJE
+		if len(dek) > 2 and len(p1inv) > 0:
+			zatvaranje_btn = Button(50,430,zatvaranje_slika, 0.5)
+			if zatvaranje_btn.draw() == True:
+				zatvaranje_tekst = "da"
+				if promjena_reda == 0:
+					zatvara = "1"
+				if promjena_reda == 1:
+					zatvara = "2"
+				dek.clear()
+
+		if zatvaranje_tekst == "da":
+			draw_text(f"ZATVARANJE!",font3,(255,255,255),300,20)
+
+		#KRAJ RUNDE	
+		if (sum(p1bodovi_runda) >= 66 or sum(p2bodovi_runda) >= 66) or ((zatvara == "1" or zatvara == "2") and len(p1inv) == 0 and len(p2inv) == 0 and sum(p1bodovi_runda) < 66 and sum(p2bodovi_runda) < 66):
+			kraj_runde_btn = Button(600,250,kraj_slika, 0.5)
 			if kraj_runde_btn.draw() == True:
 				menu_state = "kraj_runde"
 				if menu_state == "kraj_runde":
@@ -507,8 +537,15 @@ def igra():#window za igru od 7 bodova
 
 
 def pravila():#window za popis pravila igre
+	global menu_state
 	novi_prozor = pygame.display.set_mode((1024,768))
-	novi_prozor.fill(zelena)
+	novi_prozor.blit(background_slika, (0,0))
+
+	btn_povratak = Button(0,30,povratak_btn_slika,0.5)
+	if btn_povratak.draw() == True:
+		menu_state = "main"
+	if menu_state =="main":
+		main()
 
 def zvanje():
 	ogranici_zvanje = 0
@@ -527,7 +564,7 @@ def zvanje():
 			if event.type == pygame.QUIT:
 				run = False
 		novi_prozor = pygame.display.set_mode((1024,768))
-		novi_prozor.fill(zelena)
+		novi_prozor.blit(background_slika, (0,0))
 		draw_text(f"ZVANJE",font3,(255,255,255),400,80)
 		xos = 50
 		if promjena_reda == 0:
@@ -604,10 +641,6 @@ def zvanje():
 		if menu_state == "nastavi_sedam":
 			igra()
 
-		
-		
-		
-		
 		pygame.display.update()
 	pygame.quit()
 
@@ -632,6 +665,12 @@ def kraj_runde():
 	global stari_bodovi2
 	global p1pobjeda
 	global p2pobjeda
+	global zatvara
+	global zatvaranje_tekst
+	global ogranici_zvanje 
+	global ogranici_zbroj
+	global zvanje_state
+	global zvanje_baceno
 	clock = pygame.time.Clock()
 	run = True
 	while run:
@@ -640,21 +679,52 @@ def kraj_runde():
 			if event.type == pygame.QUIT:
 				run = False
 		novi_prozor = pygame.display.set_mode((1024,768))
-		novi_prozor.fill(zelena)
-		if sum(p1bodovi_runda) > sum(p2bodovi_runda) and stari_bodovi1 == p1bodovi:
-			if sum(p2bodovi_runda) == 0:
-				p1bodovi = stari_bodovi1 - 3
-			if sum(p2bodovi_runda) > 0 and sum(p2bodovi_runda) < 33:
-				p1bodovi = stari_bodovi1 - 2
-			if sum(p2bodovi_runda) > 33:
-				p1bodovi = stari_bodovi1 - 1
-		if sum(p2bodovi_runda) > sum(p1bodovi_runda) and stari_bodovi2 == p2bodovi:
-			if sum(p1bodovi_runda) == 0:
-				p2bodovi = stari_bodovi2 - 3
-			if sum(p1bodovi_runda) > 0 and sum(p1bodovi_runda) < 33:
-				p2bodovi = stari_bodovi2 - 2
-			if sum(p1bodovi_runda) > 33:
-				p2bodovi = stari_bodovi2 - 1
+		novi_prozor.blit(background_slika, (0,0))
+		if zatvara == "1" and p2bodovi == stari_bodovi2:
+			if sum(p1bodovi_runda) < 66:
+				if sum(p2bodovi_runda) == 0:
+					p2bodovi = stari_bodovi2 - 3
+				if sum(p2bodovi_runda) > 0 and sum(p2bodovi_runda) < 33:
+					p2bodovi = stari_bodovi2 - 2
+				if sum(p2bodovi_runda) > 33:
+					p2bodovi = stari_bodovi2 - 1
+				if sum(p2bodovi_runda) == 33:
+					if zadnja_karta[0][0] > zadnja_karta[0][1]:
+						p1bodovi = stari_bodovi1 - 1
+					else:
+						p2bodovi = stari_bodovi2 - 1
+			else:
+				zatvara = "0"
+		elif zatvara == "2" and p1bodovi == stari_bodovi1:
+			if sum(p2bodovi_runda) < 66:
+				if sum(p1bodovi_runda) == 0:
+					p1bodovi = stari_bodovi1 - 3
+				if sum(p1bodovi_runda) > 0 and sum(p2bodovi_runda) < 33:
+					p1bodovi = stari_bodovi1 - 2
+				if sum(p1bodovi_runda) > 33:
+					p1bodovi = stari_bodovi1 - 1
+				if sum(p2bodovi_runda) == 33:
+					if zadnja_karta[0][0] > zadnja_karta[0][1]:
+						p1bodovi = stari_bodovi1 - 1
+					else:
+						p2bodovi = stari_bodovi2 - 1
+			else:
+				zatvara = "0"
+		elif zatvara == "0":
+			if sum(p1bodovi_runda) > sum(p2bodovi_runda) and stari_bodovi1 == p1bodovi:
+				if sum(p2bodovi_runda) == 0:
+					p1bodovi = stari_bodovi1 - 3
+				if sum(p2bodovi_runda) > 0 and sum(p2bodovi_runda) < 33:
+					p1bodovi = stari_bodovi1 - 2
+				if sum(p2bodovi_runda) > 33:
+					p1bodovi = stari_bodovi1 - 1
+			if sum(p2bodovi_runda) > sum(p1bodovi_runda) and stari_bodovi2 == p2bodovi:
+				if sum(p1bodovi_runda) == 0:
+					p2bodovi = stari_bodovi2 - 3
+				if sum(p1bodovi_runda) > 0 and sum(p1bodovi_runda) < 33:
+					p2bodovi = stari_bodovi2 - 2
+				if sum(p1bodovi_runda) > 33:
+					p2bodovi = stari_bodovi2 - 1
 		novi_prozor.blit(kraj_runde_slika, (0,0))
 
 		draw_text(f"{igrač1ime}",font4,(250,250,250),550,252)
@@ -686,7 +756,11 @@ def kraj_runde():
 		usporedba_red_bacanja2.clear()
 		bacena_kartap1.clear()
 		bacena_kartap2.clear()
-
+		zatvaranje_tekst = ""
+		ogranici_zvanje = 0
+		ogranici_zbroj = 0
+		zvanje_state = "0"
+		zvanje_baceno.clear()
 		
 		if p1bodovi <= 0 or p2bodovi <= 0: #gleda je li runda gotova
 			draw_text(f"KRAJ IGRE",font3,(255,255,255),300,80)
@@ -705,6 +779,7 @@ def kraj_runde():
 				#reseta bodove runde
 				p1bodovi_runda.clear()
 				p2bodovi_runda.clear()
+				zatvara = "0"
 				main()
 		else: #nastavlja rundu ako nije gotova runda
 			draw_text(f"KRAJ RUNDE",font3,(255,255,255),300,80)
@@ -715,6 +790,7 @@ def kraj_runde():
 				#reseta bodove runde
 				p1bodovi_runda.clear()
 				p2bodovi_runda.clear()
+				zatvara = "0"
 				igra()
 		pygame.display.update()
 	pygame.quit()
@@ -731,37 +807,51 @@ def main():#loop koji pokrece igru
 			if event.type == pygame.QUIT:
 				run = False
 
-		screen.fill(zelena)
-		draw_text("ODABERI POČETAN BROJ BODOVA",font,text_color,170,100)
-		if menu_state == "main":#provjerave jesam li u glavnom menuu ili u jednom od prozora za igru
-
-			if btn_pravila.draw() == True:
-				menu_state ="pravila"
-			if btn_sedam_bodova.draw() == True:
-				menu_state ="povratak_sedam"
-			if btn_devet_bodova.draw() == True:
-				menu_state ="povratak_devet"
-
+		screen.blit(mainMenu_slika, (0,0))
+		igraj_btn = Button(290,330,igra_slika, 1)
+		pravila_btn = Button(290,450,pravila_slika, 1)
+		if igraj_btn.draw() == True:
+			menu_state = "opcije"
+		if menu_state == "opcije":
+			opcije()
+		if pravila_btn.draw() == True:
+			menu_state = "pravila"
 		if menu_state =="pravila":
 			pravila()
-			if btn_povratak.draw() == True:
-				menu_state = "main"
-
-		if menu_state == "povratak_sedam":
-			p1bodovi = 7
-			p2bodovi = 7
-			igra()
-			if btn_povratak.draw() == True:
-				menu_state = "main"
-
-		if menu_state == "povratak_devet":
-			p1bodovi = 9
-			p2bodovi = 9
-			igra()
-			if btn_povratak.draw() == True:
-				menu_state = "main"
 
 		pygame.display.update()
 	pygame.quit()
 
+def opcije():
+	global menu_state
+	global p1bodovi
+	global p2bodovi
+	clock = pygame.time.Clock()
+	run = True
+	while run:
+		clock.tick(FPS)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+		screen.blit(background_slika, (0,0))
+		sedam_bodova_btn = Button(350,400,sedam_bodova_slika,1.2)		
+		devet_bodova_btn = Button(580,400,devet_bodova_slika,1.2)
+		btn_povratak = Button(0,30,povratak_btn_slika,0.5)
+		draw_text(f"ODABERI BROJ BODOVA",font3,(255,255,255),100,200)
+		if btn_povratak.draw() == True:
+			menu_state = "main"
+		if menu_state =="main":
+			main()
+		if sedam_bodova_btn.draw() == True:
+			menu_state = "igra"
+			p1bodovi = 7
+			p2bodovi = 7
+		if menu_state == "igra":
+			igra()
+		if devet_bodova_btn.draw() == True:
+			menu_state = "igra"
+			p1bodovi = 9
+			p2bodovi = 9
+		pygame.display.update()
+	pygame.quit()
 main()
